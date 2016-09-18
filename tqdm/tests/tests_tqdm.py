f62674480a2016c18205019f6ca2ab920f198848
Stephen L <lrq3000@gmail.com>
2016-09-18 12:37:53 +0200

    Add postfix unit test + fix flake8
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/tests/tests_tqdm.py
+++ tqdm/tests/tests_tqdm.py
@@ -1274,3 +1274,30 @@ def test_autodisable_enable():
         with tqdm(total=10, disable=None, file=our_file) as t:
             pass
         assert our_file.getvalue() != ''
+
+
+@with_setup(pretest, posttest)
+def test_postfix():
+    """ Test postfix """
+    postfix = {'float': 0.321034, 'gen': 543, 'str': 'h', 'lst': [1, 2]}
+    expected = ['lst=[1, 2]', 'float=0.321', 'gen=543', 'str=h']
+
+    # Test postfix set at init
+    with closing(StringIO()) as our_file:
+        t1 = tqdm(total=10, file=our_file, desc='pos0 bar',
+                  bar_format='{r_bar}', postfix=postfix)
+        t1.refresh()
+        out = our_file.getvalue()
+
+    # Test postfix set after init
+    with closing(StringIO()) as our_file:
+        t2 = trange(10, file=our_file, desc='pos1 bar',
+                    bar_format='{r_bar}', postfix=None)
+        t2.set_postfix(**postfix)
+        t2.refresh()
+        out2 = our_file.getvalue()
+
+    # Order of items in dict may change, so need a loop to check per item
+    for res in expected:
+        assert res in out
+        assert res in out2
